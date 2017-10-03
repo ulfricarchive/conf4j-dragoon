750a8f44701f691810e41fece20ebcb22664539c
Adam Harrison <ad@mharrison.us>
2017-10-02 22:28:46 -0400

    Allow failures to happen

--- src/main/java/com/ulfric/dragoon/conf4j/Conf4jFactory.java
+++ src/main/java/com/ulfric/dragoon/conf4j/Conf4jFactory.java
@@ -18,13 +18,21 @@ import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.dragoon.qualifier.Qualifier;
 import com.ulfric.dragoon.stereotype.Stereotypes;
 
-import java.net.URISyntaxException;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URI;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conf4jFactory implements Factory {
 
@@ -33,6 +41,9 @@ public class Conf4jFactory implements Factory {
 
 	@Inject
 	private FileSystem fileSystem;
+
+	@Inject(optional = true)
+	private Logger logger;
 
 	@Override
 	public <T> T request(Class<T> type) {
@@ -56,7 +67,12 @@ public class Conf4jFactory implements Factory {
 		} else {
 			sources.add(new PathSource(existingFile));
 		}
-		sources.add(new PathSource(getClassLoaderResource(type.getClassLoader(), defaultFolder().resolve(fileName).toString())));
+
+		Path classPathResource =
+				getClassLoaderResource(type.getClassLoader(), defaultFolder().resolve(fileName).toString());
+		if (classPathResource != null) {
+			sources.add((new PathSource(classPathResource)));
+		}
 		Source source = new MultiSource(dataType, sources);
 
 		return Configuration.builder()
@@ -69,9 +85,28 @@ public class Conf4jFactory implements Factory {
 
 	private Path getClassLoaderResource(ClassLoader loader, String resource) {
 		try {
-			return Paths.get(loader.getResource(resource).toURI()); // TODO use fileSystem field
-		} catch (URISyntaxException exception) {
-			throw new RuntimeException(exception);
+			resource = resource.replace('\\', '/');
+			URI resourceUri = loader.getResource(resource).toURI();
+			getJarFileSystem(resourceUri);
+			return Paths.get(resourceUri); // TODO use fileSystem field?
+		} catch (Exception exception) {
+			if (logger != null) {
+				logger.log(Level.SEVERE, "Exception finding resource " + resource + " in loader " + loader, exception);
+			}
+			return null;
+		}
+	}
+
+	private FileSystem getJarFileSystem(URI resource) {
+		try {
+			return FileSystems.getFileSystem(resource);
+		} catch (FileSystemNotFoundException expected) {
+			Map<String, String> environment = Collections.singletonMap("create", "true");
+			try {
+				return FileSystems.newFileSystem(resource, environment);
+			} catch (IOException exception) {
+				throw new UncheckedIOException(exception);
+			}
 		}
 	}
 
